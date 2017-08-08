7eeba1ccce6cfc00a1f7fe2538e1891bbcac3f71
dmayes403 <davidrmayes@gmail.com>
2017-08-07 22:59:26 -0600

    updated boiler plate

--- server.js
+++ server.js
@@ -4,14 +4,10 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var port = 3000;
 
-var index = require('./src/index');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+// app.engine('html', require('ejs').renderFile);
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,8 +15,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // body parser setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
